146c8adf7343dee067918085ea64b706aa33cb28
Pyosangwoo <psw3619@naver.com>
2022-08-22 01:49:36 +0900

    make user model

--- SAVEARTH_BE/settings.py
+++ SAVEARTH_BE/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -110,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'ko-kr'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Asia/Seoul'
 
@@ -128,3 +129,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
--- users/views.py
+++ users/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
+from .models import User
 
 # Create your views here.
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            print('인증')
+            login(request, user)
+        else:
+            print('노노')
+    return render(request, "users/login.html")
+
+def logout_view(request):
+    logout(request)
+    return redirect("user:login")
+
+def signup_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        nickname = request.POST["nickname"]
+        birth_year = request.POST["birth_year"]
+        birth_month = request.POST["birth_month"]
+        birth_day = request.POST["birth_day"]
+        gender = request.POST["gender"]
+
+        user = User.objects.create_user(username, '', password)     
+        user.nickname = nickname
+        user.birth_year = birth_year
+        user.birth_month = birth_month
+        user.birth_day = birth_day  
+        user.gender = gender
+        user.save()
+        return redirect("user:login")
+        
+    return render(request, "users/signup.html")
