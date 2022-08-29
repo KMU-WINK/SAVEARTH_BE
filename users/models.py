adc88749cc936f1c972b09ca0f8b0a70f75dcf20
Honghyeonji <hmj2292@kookmin.ac.kr>
2022-08-29 13:09:48 +0900

    병합 전 수정

--- users/models.py
+++ users/models.py
@@ -1,19 +1,16 @@
 from django.db import models
 
-class eUser(models.Model):
-    id = models.CharField(max_length=20, primary_key=True)
-
 class Trash(models.Model):
     address = models.CharField(max_length=64, primary_key=True)
     trash_x = models.FloatField()
     trash_y = models.FloatField()
-    user_id = models.ForeignKey(eUser, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Trashcan(models.Model):
     address = models.CharField(max_length=64, primary_key=True)
     trashcan_x = models.FloatField()
     trashcan_y = models.FloatField()
-    user_id = models.ForeignKey(eUser, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Path(models.Model):
     id = models.CharField(max_length=64, primary_key= True)
@@ -21,6 +18,6 @@ class Path(models.Model):
     time = models.TimeField()
     distance = models.IntegerField()
     img = models.CharField(max_length=128)
-    user_id = models.ForeignKey(eUser, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
 # Create your models here.
