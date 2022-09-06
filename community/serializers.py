bc97bed04245e63345afa49bc0e24f179c553455
Pyosangwoo <psw3619@naver.com>
2022-09-06 23:22:05 +0900

    좋아요 누르기

--- community/serializers.py
+++ community/serializers.py
@@ -1,16 +1,21 @@
-from dataclasses import field
-from pyexpat import model
-from .models import Board, Comment
+from .models import Board, Comment, Profile
 from rest_framework import serializers
 
 class BoardSerialier(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source = 'user.nickname')
     class Meta:
         model = Board
-        fields = ['id', 'user', 'title', 'location', 'board_img', 'content', 'liked', 'comment_cnt']
+        fields = ['id', 'user', 'title', 'location', 'board_img', 'content', 'like_cnt', 'comment_cnt']
 
 class CommentSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source = 'user.nickname')
     class Meta:
         model = Comment
         fields = ['id', 'board', 'user', 'comment']
+
+class ProfileSerialier(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source = 'user.id')
+    nickname = serializers.ReadOnlyField(source = 'user.nickname')
+    class Meta:
+        model = Profile
+        fields = ['user', 'nickname']
