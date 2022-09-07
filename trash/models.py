0eaa6e5e59f34f664f4b2dde21eefdab7a5358f5
Honghyeonji <66579773+Honghyeonji@users.noreply.github.com>
2022-09-07 13:09:01 +0900

    trash remodeling, api 완성

--- trash/models.py
+++ trash/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from users.models import User
 
 class Trash(models.Model):
-    address = models.CharField(max_length=64, primary_key=True)
+    id = models.AutoField(primary_key = True, null = False, blank = False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     trash_x = models.FloatField()
     trash_y = models.FloatField()
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
