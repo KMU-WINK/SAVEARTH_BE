469673c352f7c6b1e48fcb530edb3c6ae42e266e
Honghyeonji <hmj2292@kookmin.ac.kr>
2022-09-14 23:19:05 +0900

    permission 수정

--- route/views.py
+++ route/views.py
@@ -1,20 +1,19 @@
+from route.permissions import IsOwnerOrReadOnly
 from .models import Route
 from .serializers import RouteSerializer
 from rest_framework import viewsets, parsers
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
 
 class RouteViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     queryset = Route.objects.all()
     serializer_class = RouteSerializer
 
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['user_id']
-
-    parser_classes = [parsers.MultiPartParser, parsers.FormParser]
 
     def perform_create(self, serializer):
         serializer.save(user = self.request.user)
