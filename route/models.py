b60edf00de39446b89e9638f7c6568c3ba991959
Honghyeonji <hmj2292@kookmin.ac.kr>
2022-09-14 18:04:53 +0900

    route 모델 수정

--- route/models.py
+++ route/models.py
@@ -4,8 +4,8 @@ from users.models import User
 class Route(models.Model):
     id = models.AutoField(primary_key = True, null = False, blank = False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    steps = models.IntegerField()
-    starttime = models.DateTimeField()
-    endtime = models.DateTimeField()
+    steps = models.IntegerField(null = True)
+    time = models.CharField(max_length=16)
+    datetime = models.DateTimeField()
     distance = models.IntegerField()
     image = models.ImageField(upload_to="")
