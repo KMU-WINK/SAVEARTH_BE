8b74d6475e1b964a33ad5ecd0162af9e07f58ba3
Honghyeonji <hmj2292@kookmin.ac.kr>
2022-09-15 11:15:31 +0900

    route permission연결, trash, trashcan 필요없는 코드 정리

--- route/views.py
+++ route/views.py
@@ -1,14 +1,14 @@
-from route.permissions import IsOwnerOrReadOnly
 from .models import Route
 from .serializers import RouteSerializer
-from rest_framework import viewsets, parsers
+from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
+from route.permissions import IsOwnerOrReadOnly
 
 class RouteViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
     queryset = Route.objects.all()
     serializer_class = RouteSerializer
 
